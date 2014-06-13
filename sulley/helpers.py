756257cb262a48d0c3a9a23ff827ea35b4faa922
Ryan Sears <fitblip@gmail.com>
2014-06-13 02:22:32 -0700

    Add helpers
    Add helpers, with a single helper for now that gets the max UDP packet
    size in a platform-agnostic way.

---
+++ sulley/helpers.py
@@ -0,0 +1,48 @@
+import socket
+import platform
+import ctypes as c
+
+# noinspection PyPep8Naming
+def get_max_udp_size():
+    """
+    Crazy CTypes magic to do a getsockopt() which determines the max UDP payload size in a platform-agnostic way.
+
+        @rtype:  long
+        @return: The maximum length of a UDP packet the current platform supports
+    """
+    windows = platform.uname()[0] == "Windows"
+    mac     = platform.uname()[0] == "Darwin"
+    linux   = platform.uname()[0] == "Linux"
+    lib     = None
+
+    if windows:
+        SOL_SOCKET = c.c_int(0xffff)
+        SOL_MAX_MSG_SIZE = 0x2003
+        lib = c.WinDLL('Ws2_32.dll')
+        OPT = c.c_int(SOL_MAX_MSG_SIZE)
+    elif linux or mac:
+        if mac:
+            lib = c.cdll.LoadLibrary('libc.dylib')
+        elif linux:
+            lib = c.cdll.LoadLibrary('libc.so.6')
+        SOL_SOCKET = c.c_int(socket.SOL_SOCKET)
+        OPT        = c.c_int(socket.SO_SNDBUF)
+
+    else:
+        raise Exception("Unknown platform!")
+
+    ulong_size = c.sizeof(c.c_ulong)
+    buf = c.create_string_buffer(ulong_size)
+    bufsize = c.c_int(ulong_size)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    lib.getsockopt(
+        sock.fileno(),
+        SOL_SOCKET,
+        OPT,
+        buf,
+        c.pointer(bufsize)
+    )
+
+    return c.c_ulong.from_buffer(buf).value
