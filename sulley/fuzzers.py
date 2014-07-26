348968ebfff8e8adaf757768d2ec5d8335e65696
Fitblip <fitblip@gmail.com>
2014-07-26 03:05:50 -0700

    Stubs for breaking the fuzzing engine into it's own abstraction

---
+++ sulley/fuzzers.py
@@ -0,0 +1,22 @@
+class Fuzzer(object):
+    blocks = []
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return "<Fuzzer>"
+
+class BlockBasedFuzzer(Fuzzer):
+    def __init__(self):
+        super(BlockBasedFuzzer, self).__init__()
+
+    def __repr__(self):
+        return "<BlockBasedFuzzer>"
+
+class DumbFileFuzzer(Fuzzer):
+    def __init__(self):
+        super(DumbFileFuzzer, self).__init__()
+
+    def __repr__(self):
+        return "<FileFuzzer>"
