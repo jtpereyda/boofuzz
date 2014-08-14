10976e6e3b9b5e7f765e87e2b8b1551cca7c4181
Ryan Sears <Fitblip@gmail.com>
2014-08-14 15:49:01 -0700

    Lots of syntactical changes to vmcontrol.
    Squelching warnings, takin names

--- vmcontrol.py
+++ vmcontrol.py
@@ -6,8 +6,14 @@ import sys
 import time
 import getopt
 
+if os.name != "nt":
+    print "[!] This only works on windows!"
+    sys.exit(1)
+
 try:
+    # noinspection PyUnresolvedReferences
     from win32api import GetShortPathName
+    # noinspection PyUnresolvedReferences
     from win32com.shell import shell
 except:
     if os.name == "nt":
@@ -19,34 +25,34 @@ from sulley import pedrpc
 
 PORT  = 26003
 ERR   = lambda msg: sys.stderr.write("ERR> " + msg + "\n") or sys.exit(1)
-USAGE = "USAGE: vmcontrol.py"                                                             \
+USAGE = "USAGE: vmcontrol.py"                                                               \
         "\n    <-x|--vmx FILENAME|NAME> path to VMX to control or name of VirtualBox image" \
         "\n    <-r|--vmrun FILENAME>    path to vmrun.exe or VBoxManage"                    \
         "\n    [-s|--snapshot NAME>     set the snapshot name"                              \
         "\n    [-l|--log_level LEVEL]   log level (default 1), increase for more verbosity" \
         "\n    [-i|--interactive]       Interactive mode, prompts for input values"         \
-        "\n    [--port PORT]            TCP port to bind this agent to"                      \
+        "\n    [--port PORT]            TCP port to bind this agent to"                     \
         "\n    [--vbox]                 control an Oracle VirtualBox VM"
 
-########################################################################################################################
-class vmcontrol_pedrpc_server (pedrpc.Server):
+
+class VMControlPedrpcServer (pedrpc.Server):
     def __init__(self, host, port, vmrun, vmx, snap_name=None, log_level=1, interactive=False):
-        '''
-        @type  host:         String
+        """
+        @type  host:         str
         @param host:         Hostname or IP address to bind server to
-        @type  port:         Integer
+        @type  port:         int
         @param port:         Port to bind server to
-        @type  vmrun:        String
+        @type  vmrun:        str
         @param vmrun:        Path to VMWare vmrun.exe
-        @type  vmx:          String
+        @type  vmx:          str
         @param vmx:          Path to VMX file
-        @type  snap_name:    String
+        @type  snap_name:    str
         @param snap_name:    (Optional, def=None) Snapshot name to revert to on restart
-        @type  log_level:    Integer
+        @type  log_level:    int
         @param log_level:    (Optional, def=1) Log output level, increase for more verbosity
-        @type  interactive:  Boolean
+        @type  interactive:  bool
         @param interactive:  (Option, def=False) Interactive mode, prompts for input values
-        '''
+        """
 
         # initialize the PED-RPC server.
         pedrpc.Server.__init__(self, host, port)
@@ -63,7 +69,11 @@ class vmcontrol_pedrpc_server (pedrpc.Server):
             try:
                 while 1:
                     print "[*] Please browse to the folder containing vmrun.exe..."
-                    pidl, disp, imglist = shell.SHBrowseForFolder(0, None, "Please browse to the folder containing vmrun.exe:")
+                    pidl, disp, imglist = shell.SHBrowseForFolder(
+                        0,
+                        None,
+                        "Please browse to the folder containing vmrun.exe:"
+                    )
                     fullpath = shell.SHGetPathFromIDList(pidl)
                     file_list = os.listdir(fullpath)
                     if "vmrun.exe" not in file_list:
@@ -80,16 +90,20 @@ class vmcontrol_pedrpc_server (pedrpc.Server):
             try:
                 while 1:
                     print "[*] Please browse to the folder containing the .vmx file..."
-                    pidl, disp, imglist = shell.SHBrowseForFolder(0, None, "Please browse to the folder containing the .vmx file:")
+                    pidl, disp, imglist = shell.SHBrowseForFolder(
+                        0,
+                        None,
+                        "Please browse to the folder containing the .vmx file:"
+                    )
                     fullpath = shell.SHGetPathFromIDList(pidl)
                     file_list = os.listdir(fullpath)
 
                     exists = False
-                    for file in file_list:
-                        idx = file.find(".vmx")
-                        if idx == len(file) - 4:
+                    for filename in file_list:
+                        idx = filename.find(".vmx")
+                        if idx == len(filename) - 4:
                             exists = True
-                            vmx = fullpath + "\\" + file
+                            vmx = fullpath + "\\" + filename
                             print "[*] Using %s" % vmx
 
                     if exists:
@@ -97,7 +111,6 @@ class vmcontrol_pedrpc_server (pedrpc.Server):
                     else:
                         print "[!] No .vmx file found in the selected folder, please try again"
             except:
-                raise
                 print "[!] Error while trying to find the .vmx file. Try again without -I."
                 sys.exit(1)
 
@@ -130,49 +143,44 @@ class vmcontrol_pedrpc_server (pedrpc.Server):
         self.log("\t log level: %d" % self.log_level)
         self.log("Awaiting requests...")
 
-
+    # noinspection PyMethodMayBeStatic
     def alive(self):
-        '''
+        """
         Returns True. Useful for PED-RPC clients who want to see if the PED-RPC connection is still alive.
-        '''
-
+        """
         return True
 
-
     def log(self, msg="", level=1):
-        '''
+        """
         If the supplied message falls under the current log level, print the specified message to screen.
 
-        @type  msg: String
+        @type  msg: str
         @param msg: Message to log
-        '''
+        """
 
         if self.log_level >= level:
             print "[%s] %s" % (time.strftime("%I:%M.%S"), msg)
-
 
     def set_vmrun(self, vmrun):
         self.log("setting vmrun to %s" % vmrun, 2)
         self.vmrun = vmrun
 
-
     def set_vmx(self, vmx):
         self.log("setting vmx to %s" % vmx, 2)
         self.vmx = vmx
-
 
     def set_snap_name(self, snap_name):
         self.log("setting snap_name to %s" % snap_name, 2)
         self.snap_name = snap_name
 
-
     def vmcommand(self, command):
-        '''
+        """
         Execute the specified command, keep trying in the event of a failure.
 
-        @type  command: String
+        @type  command: str
         @param command: VMRun command to execute
-        '''
+        """
+        out = None
 
         while 1:
             self.log("executing: %s" % command, 5)
@@ -195,12 +203,6 @@ class vmcontrol_pedrpc_server (pedrpc.Server):
 
         return "".join(out)
 
-
-    ###
-    ### VMRUN COMMAND WRAPPERS
-    ###
-
-
     def delete_snapshot(self, snap_name=None):
         if not snap_name:
             snap_name = self.snap_name
@@ -210,13 +212,11 @@ class vmcontrol_pedrpc_server (pedrpc.Server):
         command = self.vmrun + " deleteSnapshot " + self.vmx + " " + '"' + snap_name + '"'
         return self.vmcommand(command)
 
-
     def list(self):
         self.log("listing running images", 2)
 
         command = self.vmrun + " list"
         return self.vmcommand(command)
-
 
     def list_snapshots(self):
         self.log("listing snapshots", 2)
@@ -224,13 +224,11 @@ class vmcontrol_pedrpc_server (pedrpc.Server):
         command = self.vmrun + " listSnapshots " + self.vmx
         return self.vmcommand(command)
 
-
     def reset(self):
         self.log("resetting image", 2)
 
         command = self.vmrun + " reset " + self.vmx
         return self.vmcommand(command)
-
 
     def revert_to_snapshot(self, snap_name=None):
         if not snap_name:
@@ -240,7 +238,6 @@ class vmcontrol_pedrpc_server (pedrpc.Server):
 
         command = self.vmrun + " revertToSnapshot " + self.vmx + " " + '"' + snap_name + '"'
         return self.vmcommand(command)
-
 
     def snapshot(self, snap_name=None):
         if not snap_name:
@@ -252,13 +249,11 @@ class vmcontrol_pedrpc_server (pedrpc.Server):
 
         return self.vmcommand(command)
 
-
     def start(self):
         self.log("starting image", 2)
 
         command = self.vmrun + " start " + self.vmx
         return self.vmcommand(command)
-
 
     def stop(self):
         self.log("stopping image", 2)
@@ -266,18 +261,11 @@ class vmcontrol_pedrpc_server (pedrpc.Server):
         command = self.vmrun + " stop " + self.vmx
         return self.vmcommand(command)
 
-
     def suspend(self):
         self.log("suspending image", 2)
 
         command = self.vmrun + " suspend " + self.vmx
         return self.vmcommand(command)
-
-
-    ###
-    ### EXTENDED COMMANDS
-    ###
-
 
     def restart_target(self):
         self.log("restarting virtual machine...")
@@ -288,7 +276,6 @@ class vmcontrol_pedrpc_server (pedrpc.Server):
 
         # wait for the snapshot to come alive.
         self.wait()
-
 
     def is_target_running(self):
         # sometimes vmrun reports that the VM is up while it's still reverting.
@@ -307,7 +294,6 @@ class vmcontrol_pedrpc_server (pedrpc.Server):
 
         return False
 
-
     def wait(self):
         self.log("waiting for vmx to come up: %s" % self.vmx)
         while 1:
@@ -315,29 +301,26 @@ class vmcontrol_pedrpc_server (pedrpc.Server):
                 break
 
 
-########################################################################################################################
-
-########################################################################################################################
-class vboxcontrol_pedrpc_server (vmcontrol_pedrpc_server):
+class VBoxControlPedrpcServer (VMControlPedrpcServer):
     def __init__(self, host, port, vmrun, vmx, snap_name=None, log_level=1, interactive=False):
-        '''
+        """
         Controls an Oracle VirtualBox Virtual Machine
-        
-        @type  host:         String
+
+        @type  host:         str
         @param host:         Hostname or IP address to bind server to
-        @type  port:         Integer
+        @type  port:         int
         @param port:         Port to bind server to
-        @type  vmrun:        String
+        @type  vmrun:        str
         @param vmrun:        Path to VBoxManage
-        @type  vmx:          String
+        @type  vmx:          str
         @param vmx:          Name of the virtualbox VM to control (no quotes)
-        @type  snap_name:    String
+        @type  snap_name:    str
         @param snap_name:    (Optional, def=None) Snapshot name to revert to on restart
-        @type  log_level:    Integer
+        @type  log_level:    int
         @param log_level:    (Optional, def=1) Log output level, increase for more verbosity
-        @type  interactive:  Boolean
+        @type  interactive:  bool
         @param interactive:  (Option, def=False) Interactive mode, prompts for input values
-        '''
+        """
 
         # initialize the PED-RPC server.
         pedrpc.Server.__init__(self, host, port)
@@ -346,9 +329,7 @@ class vboxcontrol_pedrpc_server (vmcontrol_pedrpc_server):
         self.port        = port
 
         self.interactive = interactive
- 
 
-    
         if interactive:
             print "[*] Entering interactive mode..."
 
@@ -356,7 +337,11 @@ class vboxcontrol_pedrpc_server (vmcontrol_pedrpc_server):
             try:
                 while 1:
                     print "[*] Please browse to the folder containing VBoxManage.exe..."
-                    pidl, disp, imglist = shell.SHBrowseForFolder(0, None, "Please browse to the folder containing VBoxManage.exe")
+                    pidl, disp, imglist = shell.SHBrowseForFolder(
+                        0,
+                        None,
+                        "Please browse to the folder containing VBoxManage.exe"
+                    )
                     fullpath = shell.SHGetPathFromIDList(pidl)
                     file_list = os.listdir(fullpath)
                     if "VBoxManage.exe" not in file_list:
@@ -369,29 +354,22 @@ class vboxcontrol_pedrpc_server (vmcontrol_pedrpc_server):
                 print "[!] Error while trying to find VBoxManage.exe. Try again without -I."
                 sys.exit(1)
 
-
-
         # Grab vmx, snapshot name and log level if we're in interactive mode
-
         if interactive:
             vmx = raw_input("[*] Please enter the VirtualBox virtual machine name: ")
             snap_name = raw_input("[*] Please enter the snapshot name: ")
             log_level = raw_input("[*] Please enter the log level (default 1): ")
-    
 
         if log_level:
             log_level = int(log_level)
         else:
             log_level = 1
 
-
         # if we're on windows, get the DOS path names
-
         if os.name == "nt":
             self.vmrun = GetShortPathName(r"%s" % vmrun)
             self.vmx   = GetShortPathName(r"%s" % vmx)
         else:
-    
             self.vmrun = vmrun
             self.vmx   = vmx
 
@@ -406,11 +384,6 @@ class vboxcontrol_pedrpc_server (vmcontrol_pedrpc_server):
         self.log("\t log level: %d" % self.log_level)
         self.log("Awaiting requests...")
 
-    ###
-    ### VBOXMANAGE COMMAND WRAPPERS
-    ###
-
-
     def delete_snapshot(self, snap_name=None):
         if not snap_name:
             snap_name = self.snap_name
@@ -420,20 +393,17 @@ class vboxcontrol_pedrpc_server (vmcontrol_pedrpc_server):
         command = self.vmrun + " snapshot " + self.vmx + " delete " + snap_name + '"'
         return self.vmcommand(command)
 
-
     def list(self):
         self.log("listing running images", 2)
 
         command = self.vmrun + " list runningvms"
         return self.vmcommand(command)
 
-
     def list_snapshots(self):
         self.log("listing snapshots", 2)
 
         command = self.vmrun + " snapshot " + self.vmx + " list"
         return self.vmcommand(command)
-
 
     def reset(self):
         self.log("resetting image", 2)
@@ -461,13 +431,10 @@ class vboxcontrol_pedrpc_server (vmcontrol_pedrpc_server):
         if self.is_target_running():
             self.stop()
 
-
-
         self.log("reverting to snapshot: %s" % snap_name, 2)
 
         command = self.vmrun + " snapshot " + self.vmx + " restore " + snap_name 
         return self.vmcommand(command)
-
 
     def snapshot(self, snap_name=None):
         if not snap_name:
@@ -475,15 +442,13 @@ class vboxcontrol_pedrpc_server (vmcontrol_pedrpc_server):
 
         #VirtualBox flips out if you try to do this with a running VM
         if self.is_target_running():
-               self.pause()
-
+            self.pause()
 
         self.log("taking snapshot: %s" % snap_name, 2)
 
         command = self.vmrun + " snapshot " + self.vmx + " take " + snap_name
 
         return self.vmcommand(command)
-
 
     def start(self):
         self.log("starting image", 2)
@@ -492,13 +457,11 @@ class vboxcontrol_pedrpc_server (vmcontrol_pedrpc_server):
         # TODO: we may want to do more here with headless, gui, etc...
         return self.vmcommand(command)
 
-
     def stop(self):
         self.log("stopping image", 2)
 
         command = self.vmrun + " controlvm " + self.vmx + " poweroff"
         return self.vmcommand(command)
-
 
     def suspend(self):
         self.log("suspending image", 2)
@@ -506,19 +469,12 @@ class vboxcontrol_pedrpc_server (vmcontrol_pedrpc_server):
         command = self.vmrun + " controlvm " + self.vmx + " pause"
         return self.vmcommand(command)
 
-
-    ###
-    ### EXTENDED COMMANDS
-    ###
-
-#added a function here to get vminfo... useful for parsing stuff out later
+    #added a function here to get vminfo... useful for parsing stuff out later
     def get_vminfo(self):
+        self.log("getting vminfo", 2)
 
-           self.log("getting vminfo", 2)
-
-           command = self.vmrun + " showvminfo " + self.vmx + " --machinereadable"
-           return self.vmcommand(command)
-
+        command = self.vmrun + " showvminfo " + self.vmx + " --machinereadable"
+        return self.vmcommand(command)
 
     def restart_target(self):
         self.log("restarting virtual machine...")
@@ -533,7 +489,6 @@ class vboxcontrol_pedrpc_server (vmcontrol_pedrpc_server):
 
         # wait for the snapshot to come alive.
         self.wait()
-
 
     def is_target_running(self):
         # sometimes vmrun reports that the VM is up while it's still reverting.
@@ -554,43 +509,77 @@ class vboxcontrol_pedrpc_server (vmcontrol_pedrpc_server):
 
         return False
 
-
-########################################################################################################################
-
 if __name__ == "__main__":
+    opts = None
+
+    vmrun_arg       = r"C:\progra~1\vmware\vmware~1\vmrun.exe"
+    vmx_arg         = None
+    snap_name_arg   = None
+    log_level_arg   = 1
+    interactive_arg = False
+    virtualbox_arg  = False
+    port_arg        = None
+
     # parse command line options.
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "x:r:s:l:i", ["vmx=", "vmrun=", "snapshot=", "log_level=", "interactive", "port=", "vbox"])
+        opts, args = getopt.getopt(
+            sys.argv[1:],
+            "x:r:s:l:i", [
+                "vmx=",
+                "vmrun=",
+                "snapshot=",
+                "log_level=",
+                "interactive",
+                "port=",
+                "vbox"
+            ]
+        )
     except getopt.GetoptError:
         ERR(USAGE)
-
-    vmrun       = r"C:\progra~1\vmware\vmware~1\vmrun.exe"
-    vmx         = None
-    snap_name   = None
-    log_level   = 1
-    interactive = False
-    virtualbox  = False
     
     for opt, arg in opts:
-        if opt in ("-x", "--vmx"):         vmx         = arg
-        if opt in ("-r", "--vmrun"):       vmrun       = arg
-        if opt in ("-s", "--snapshot"):    snap_name   = arg
-        if opt in ("-l", "--log_level"):   log_level   = int(arg)
-        if opt in ("-i", "--interactive"): interactive = True
-        if opt in ("--port"):              PORT        = int(arg)
-        if opt in ("--vbox"):              virtualbox  = True
+        if opt in ("-x", "--vmx"):
+            vmx_arg = arg
+        if opt in ("-r", "--vmrun"):
+            vmrun_arg = arg
+        if opt in ("-s", "--snapshot"):
+            snap_name_arg = arg
+        if opt in ("-l", "--log_level"):
+            log_level_arg = int(arg)
+        if opt in ("-i", "--interactive"):
+            interactive_arg = True
+        if opt in ("-p", "--port"):
+            port_arg = int(arg)
+        if opt in ("-v", "--vbox"):
+            virtualbox_arg = True
         
     # OS check
-    if interactive and not os.name == "nt":
+    if interactive_arg and not os.name == "nt":
         print "[!] Interactive mode currently only works on Windows operating systems."
         ERR(USAGE)
 
-    if not vmx and not interactive:
+    if not vmx_arg and not interactive_arg:
         ERR(USAGE)
     
-    if not virtualbox:
-        servlet = vmcontrol_pedrpc_server("0.0.0.0", PORT, vmrun, vmx, snap_name, log_level, interactive)
-    elif virtualbox:
-        servlet = vboxcontrol_pedrpc_server("0.0.0.0", PORT, vmrun, vmx, snap_name, log_level, interactive)
+    if not virtualbox_arg:
+        servlet = VMControlPedrpcServer(
+            "0.0.0.0",
+            port_arg,
+            vmrun_arg,
+            vmx_arg,
+            snap_name_arg,
+            log_level_arg,
+            interactive_arg
+        )
+    else:
+        servlet = VBoxControlPedrpcServer(
+            "0.0.0.0",
+            port_arg,
+            vmrun_arg,
+            vmx_arg,
+            snap_name_arg,
+            log_level_arg,
+            interactive_arg
+        )
     
     servlet.serve_forever()
