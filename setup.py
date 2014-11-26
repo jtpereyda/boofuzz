5e3355cc22c29684b8dbcd923daddb2c278aee04
Anna Moshkina <moshkina@undev.ru>
2014-11-26 18:33:05 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='Sulley',
+    download_url='https://github.com/OpenRCE/sulley',
+    packages=['requests', 'sulley', 'sulley.legos', 'sulley.pgraph', 'sulley.utils',
+              'unit_tests', 'utils', 'web'],
+    package_dir={'requests': './requests',
+                 'sulley': './sulley',
+                 'sulley.legos': './sulley/legos',
+                 'sulley.pgraph': './sulley/pgraph',
+                 'sulley.utils': './sulley/utils',
+                 'unit_tests': './unit_tests',
+                 'utils': './utils',
+                 'web': './web'
+                 },
+    install_requires=['pydot2==1.0.33', 'tornado==4.0.2', 'Flask==0.10.1']
+)
