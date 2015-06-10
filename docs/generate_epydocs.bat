829758b9ec3aa43ab909d9c50173b10dce83c159
Steve Martin <sjmart1uk@yahoo.co.uk>
2015-06-10 21:29:25 +0100

    Add "offset" parameter to the s_size funtion. TL;DR I had a block whereby the whole block contents had to be considered for checksum calculation, but one field (fixed length) not included in the size field for the block. Detail: I was creating a fuzzer for PNG fields whose "Chunks" required the above consideration, see: http://www.w3.org/TR/PNG/#5Chunk-layout

--- docs/generate_epydocs.bat
+++ docs/generate_epydocs.bat
@@ -1,1 +1,1 @@
-c:\Python26\python.exe c:\Python26\Scripts\epydoc -o Sulley --css blue --name "Sulley: Fuzzing Framework" --url "http://pedram.openrce.org" ..\sulley
+c:\Python27\python.exe c:\Python27\Scripts\epydoc.py -o Sulley --css blue --name "Sulley: Fuzzing Framework" --url "http://pedram.openrce.org" ..\sulley
