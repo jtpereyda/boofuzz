58e03bde91a4910a34e44051eef55ba2658425ab
Joshua Pereyda <jtpereyda@users.noreply.github.com>
2016-01-08 14:54:38 -0800

    FUZZ-234 Changed version number to match PEP 0440. Moved to __init__.py (setup.py reads it dynamically).

--- setup.py
+++ setup.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python
+import ast
+import os
+import re
+
 from setuptools import setup
+
+setup_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+def find_version(*path_elements):
+    """Search a file for `__version__ = 'version number'` and return version.
+
+    @param path_elements: Arguments specifying file to search.
+
+    @return: Version number string.
+    """
+    path = os.path.join(setup_dir, *path_elements)
+    for line in open(path):
+        for match in re.finditer('__version__\s*=\s(.*)$', line):
+            return ast.literal_eval(match.group(1))
+    raise RuntimeError("version string not found in {0}".format(path))
+
 
 setup(
         name='boofuzz',
-        version='0.0.1.dev.1',
+        version=find_version("boofuzz", "__init__.py"),
         maintainer='Joshua Pereyda',
         maintainer_email='joshua.t.pereyda@gmail.com',
         url='https://github.com/jtpereyda/boofuzz',
