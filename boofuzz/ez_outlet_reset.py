ce7e2294bfab3c5ba7994823e2e4cd0527301bf9
Joshua Pereyda <joshua_pereyda@selinc.com>
2016-01-27 16:53:51 -0800

    FUZZ-247 Made EzOutletReset runnable; need UTs. Minor refactors in class internals.

--- boofuzz/ez_outlet_reset.py
+++ boofuzz/ez_outlet_reset.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import time
 import urllib2
@@ -5,6 +6,11 @@ import urlparse
 import ifuzz_logger
 
 import sex
+
+
+HELP_TEXT = 'Send reset command to ezOutlet EZ-11b device.'
+HELP_TEXT_TARGET_ARG = 'IP address/hostname of ezOUtlet device.'
+HELP_TEXT_TIME_ARG = 'Time in seconds to wait for UUT to reset.'
 
 
 def _get_url(hostname, path):
@@ -50,13 +56,9 @@ class EzOutletReset:
         return _get_url(self._hostname, self.RESET_URL_PATH)
 
     def post_fail(self, logger, *args, **kwargs):
-        """Tries to reset device over HTTP and wait before returning.
+        """Call reset() and log actions.
 
-        After sending HTTP request and receiving response, wait
-        self._reset_delay + self._dut_reset_time seconds.
-
-        If the outlet does not respond (after self._timeout seconds), this
-        method will raise an exception.
+        See reset() docstring for details.
 
         This method will log actions associated with HTTP communication. It
         assumes that a test step is already opened.
@@ -86,35 +88,84 @@ class EzOutletReset:
             raise
 
     def reset(self):
-        """Send reset request to ezOutlet device and wait for reset.
+        """Send reset request to ezOutlet, check response, wait for reset.
+
+        After sending HTTP request and receiving response, wait
+        self._reset_delay + self._dut_reset_time seconds.
+
+        If the outlet does not respond (after self._timeout seconds), or gives
+        an unexpected response, this method will raise an exception.
 
         Returns: HTTP response contents.
 
-        Raises sex.SullyRuntimeError if device does not respond as expected.
+        Raises:
+            sex.SulleyRuntimeError: If the reset fails due to:
+                - no response in self._timeout seconds or
+                - unexpected response contents (see
+                  EzOutletReset.EXPECTED_RESPONSE_CONTENTS)
         """
         response = self._http_get(self.url)
 
-        self._check_response(response)
+        self._check_response_raise_if_unexpected(response)
 
         self._wait_for_reset()
 
         return response
 
     def _http_get(self, url):
-        target_connection = self._connect_target(url)
-        return target_connection.read()
+        """HTTP GET and return response.
 
-    def _connect_target(self, url):
+        Args:
+            url: Target to GET.
+
+        Returns: Response contents.
+
+        Raises:
+            sex.SulleyRuntimeError: If the reset fails due to:
+                - no response in self._timeout seconds
+        """
         try:
-            return urllib2.urlopen(url, timeout=self._timeout)
+            return urllib2.urlopen(url, timeout=self._timeout).read()
         except urllib2.URLError:
             raise sex.SullyRuntimeError(self.NO_RESPONSE_MSG.format(self._timeout)), \
                 None, \
                 sys.exc_info()[2]
 
-    def _check_response(self, response):
+    def _check_response_raise_if_unexpected(self, response):
+        """Raise if response is unexpected.
+
+        Args:
+            response: Response.
+
+        Returns: None
+        """
         if response != self.EXPECTED_RESPONSE_CONTENTS:
             raise sex.SullyRuntimeError(self.UNEXPECTED_RESPONSE_MSG.format(response))
 
     def _wait_for_reset(self):
+        """Sleep for self._reset_delay + self._dut_reset_time.
+
+        Returns: None
+        """
         time.sleep(self._reset_delay + self._dut_reset_time)
+
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser(description=HELP_TEXT)
+    parser.add_argument('target', help=HELP_TEXT_TARGET_ARG)
+    parser.add_argument('--reset-time', '-t',
+                        type=float,
+                        default=0,
+                        help=HELP_TEXT_TIME_ARG)
+    return parser.parse_args(argv[1:])
+
+
+def main(argv):
+    parsed_args = parse_args(argv)
+    ez_outlet = EzOutletReset(hostname=parsed_args.target,
+                              dut_reset_time=parsed_args.reset_time)
+    ez_outlet.reset()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
