654800b7a03fbeb88f2e4c37a47619ec023d3945
Joshua Pereyda <jtpereyda@users.noreply.github.com>
2016-04-12 15:29:28 -0700

    Debugging deployment

--- boofuzz/__init__.py
+++ boofuzz/__init__.py
@@ -28,7 +28,7 @@ from .sessions import Session, Target
 from .sex import SullyRuntimeError, SizerNotUtilizedError, MustImplementException
 from .socket_connection import SocketConnection
 
-__version__ = '0.0.3.dev7'
+__version__ = '0.0.3.dev8'
 
 
 # REQUEST MANAGEMENT
