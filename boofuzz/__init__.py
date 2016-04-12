b8a35890eb3ce1ae630d30645211867018c0fcd6
Joshua Pereyda <jtpereyda@users.noreply.github.com>
2016-04-12 13:30:58 -0700

    Debugging deployment: moved deploy.distributions option

--- boofuzz/__init__.py
+++ boofuzz/__init__.py
@@ -28,7 +28,7 @@ from .sessions import Session, Target
 from .sex import SullyRuntimeError, SizerNotUtilizedError, MustImplementException
 from .socket_connection import SocketConnection
 
-__version__ = '0.0.3.dev4'
+__version__ = '0.0.3.dev5'
 
 
 # REQUEST MANAGEMENT
