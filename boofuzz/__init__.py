382aee59c69c34d321c29766182d300dede1befc
Joshua Pereyda <jtpereyda@users.noreply.github.com>
2016-04-12 17:41:52 -0700

    Debugging deployment: change password

--- boofuzz/__init__.py
+++ boofuzz/__init__.py
@@ -28,7 +28,7 @@ from .sessions import Session, Target
 from .sex import SullyRuntimeError, SizerNotUtilizedError, MustImplementException
 from .socket_connection import SocketConnection
 
-__version__ = '0.0.3.dev10'
+__version__ = '0.0.3.dev11'
 
 
 # REQUEST MANAGEMENT
