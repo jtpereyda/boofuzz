jtpereyda/boofuzz

commit 81e5d61bbcdf1cb6ade20f595a670aa84759afd5

FUZZ-257 Added MANIFEST.in and classifiers to setup.py

setup.py
@@ -37,4 +37,17 @@ setup(
         extras_require={
             'dev': ['mock', 'pytest', 'pytest-bdd'],
         },
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: English',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Security',
+            'Topic :: Software Development :: Testing :: Traffic Generation',
+        ]
 )

MANIFEST.in
@@ -1,0 +1,12 @@
+include *.md
+include *.py
+include *.txt
+recursive-include boofuzz *.md
+recursive-include boofuzz *.py
+recursive-include examples *.py
+recursive-include new_examples *.py
+recursive-include requests *.html
+recursive-include requests *.py
+recursive-include unit_tests *.feature
+recursive-include unit_tests *.py
+recursive-include utils *.py

commit 16d49cdf77342373e4229b384608a430f27ae1c6

Fix setup.py to not include superfluous packages
This was a regression from 81e5d61bbcdf1cb6ade20f595a670aa84759afd5, when
we switched to using find_packages.

setup.py
@@ -29,7 +29,7 @@ setup(
         maintainer_email='joshua.t.pereyda@gmail.com',
         url='https://github.com/jtpereyda/boofuzz',
         license='GPL',
-        packages=find_packages(exclude=['unit_tests']),
+        packages=find_packages(exclude=['unit_tests', 'requests', 'examples', 'utils', 'web', 'new_examples']),
         package_data={'boofuzz': ['web/templates/*', 'web/static/css/*']},
         install_requires=[
             'future', 'pyserial', 'pydot2==1.0.33', 'tornado==4.0.2',