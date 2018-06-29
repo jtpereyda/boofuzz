027e08fbf7159ce31b1c65b1964c336a2c79c683
Joshua Pereyda <jtpereyda@users.noreply.github.com>
2018-06-28 22:22:53 -0700

    FuzzLoggerDb now commits properly; added FuzzLoggerDbReader

--- boofuzz/fuzz_logger_db.py
+++ boofuzz/fuzz_logger_db.py
@@ -29,25 +29,20 @@ def get_time_stamp():
 
 
 class FuzzLoggerDb(ifuzz_logger_backend.IFuzzLoggerBackend):
-    """Log fuzz data in a sqlite database file.
+    """Log fuzz data in a sqlite database file."""
 
-    Args:
-        bytes_to_str (function): Function that converts sent/received bytes data to string for logging.
-    """
-
-    def __init__(self, bytes_to_str=DEFAULT_HEX_TO_STR):
+    def __init__(self):
         timestamp = datetime.datetime.utcnow().replace(microsecond=0).isoformat().replace(':', '-')
-        self._databaes_connection = sqlite3.connect('boofuzz-run-{0}.db'.format(timestamp), check_same_thread=False)
-        self._db_cursor = self._databaes_connection.cursor()
+        self._database_connection = sqlite3.connect('boofuzz-run-{0}.db'.format(timestamp), check_same_thread=False)
+        self._db_cursor = self._database_connection.cursor()
         self._db_cursor.execute('''CREATE TABLE cases (name text, number integer, timestamp TEXT)''')
         self._db_cursor.execute(
             '''CREATE TABLE steps (test_case_index integer, type text, description text, data blob, timestamp TEXT)''')
 
-        self._format_raw_bytes = bytes_to_str
         self._current_test_case_index = None
 
     def get_test_case_data(self, index):
-        c = self._databaes_connection.cursor()
+        c = self._database_connection.cursor()
         test_case_row = next(c.execute('''SELECT * FROM cases WHERE number=?''', [index]))
         rows = c.execute('''SELECT * FROM steps WHERE test_case_index=?''', [index])
         steps = []
@@ -70,35 +65,77 @@ class FuzzLoggerDb(ifuzz_logger_backend.IFuzzLoggerBackend):
     def open_test_case(self, test_case_id, name, index, *args, **kwargs):
         self._db_cursor.execute('''INSERT INTO cases VALUES(?, ?, ?)''', [name, index, helpers.get_time_stamp()])
         self._current_test_case_index = index
+        self._database_connection.commit()
 
     def open_test_step(self, description):
         self._db_cursor.execute('''INSERT INTO steps VALUES(?, ?, ?, ?, ?)''',
                                 [self._current_test_case_index, 'step', description, b'', helpers.get_time_stamp()])
+        self._database_connection.commit()
 
     def log_check(self, description):
         self._db_cursor.execute('''INSERT INTO steps VALUES(?, ?, ?, ?, ?)''',
                                 [self._current_test_case_index, 'check', description, b'', helpers.get_time_stamp()])
+        self._database_connection.commit()
 
     def log_error(self, description):
         self._db_cursor.execute('''INSERT INTO steps VALUES(?, ?, ?, ?, ?)''',
                                 [self._current_test_case_index, 'error', description, b'', helpers.get_time_stamp()])
+        self._database_connection.commit()
 
     def log_recv(self, data):
         self._db_cursor.execute('''INSERT INTO steps VALUES(?, ?, ?, ?, ?)''',
                                 [self._current_test_case_index, 'receive', u'', buffer(data), helpers.get_time_stamp()])
+        self._database_connection.commit()
 
     def log_send(self, data):
         self._db_cursor.execute('''INSERT INTO steps VALUES(?, ?, ?, ?, ?)''',
                                 [self._current_test_case_index, 'send', u'', buffer(data), helpers.get_time_stamp()])
+        self._database_connection.commit()
 
     def log_info(self, description):
         self._db_cursor.execute('''INSERT INTO steps VALUES(?, ?, ?, ?, ?)''',
                                 [self._current_test_case_index, 'info', description, b'', helpers.get_time_stamp()])
+        self._database_connection.commit()
 
     def log_fail(self, description=""):
         self._db_cursor.execute('''INSERT INTO steps VALUES(?, ?, ?, ?, ?)''',
                                 [self._current_test_case_index, 'fail', description, b'', helpers.get_time_stamp()])
+        self._database_connection.commit()
 
     def log_pass(self, description=""):
         self._db_cursor.execute('''INSERT INTO steps VALUES(?, ?, ?, ?, ?)''',
                                 [self._current_test_case_index, 'pass', description, b'', helpers.get_time_stamp()])
+        self._database_connection.commit()
+
+
+class FuzzLoggerDbReader(object):
+    """Read fuzz data saved using FuzzLoggerDb
+
+    Args:
+        db_filename (str): Name of database file to read.
+    """
+
+    def __init__(self, db_filename):
+        self._database_connection = sqlite3.connect(db_filename, check_same_thread=False)
+        self._db_cursor = self._database_connection.cursor()
+
+    def get_test_case_data(self, index):
+        c = self._database_connection.cursor()
+        test_case_row = next(c.execute('''SELECT * FROM cases WHERE number=?''', [index]))
+        rows = c.execute('''SELECT * FROM steps WHERE test_case_index=?''', [index])
+        steps = []
+        for row in rows:
+            data = row[3]
+            # Little hack since BLOB becomes type buffer in py2 and bytes in py3
+            # At the end, data will be equivalent types: bytes in py3 and str in py2
+            try:
+                if isinstance(data, buffer):
+                    data = str(data)
+            except NameError as e:
+                if 'buffer' in str(e):  # buffer type does not exist in py3
+                    pass
+                else:
+                    raise
+            steps.append(test_step_data.TestStepData(type=row[1], description=row[2], data=data, timestamp=row[4]))
+        return test_case_data.TestCaseData(name=test_case_row[0], index=test_case_row[1], timestamp=test_case_row[2],
+                                           steps=steps)
