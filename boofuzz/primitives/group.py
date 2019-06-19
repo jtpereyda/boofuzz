db638651392ee8503e3db27066fd940e032a8475
Jan Stárek <starek4@centrum.cz>
2019-06-19 15:08:37 +0200

    Do not ignore empty default value

--- boofuzz/primitives/group.py
+++ boofuzz/primitives/group.py
@@ -24,8 +24,6 @@ class Group(BasePrimitive):
 
         assert len(self.values) > 0, "You can't have an empty value list for your group!"
 
-        if not default_value:
-            default_value = self.values[0]
         self._value = self._original_value = default_value
 
         for val in self.values:
