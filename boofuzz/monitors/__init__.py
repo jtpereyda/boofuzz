1ed0496edbc864ee5842c38fbbbcdbbdf9cc6bb1
Katharina Bogad <katharina@bogad.at>
2020-02-14 18:29:55 +0100

    moved external monitor

---
+++ boofuzz/monitors/__init__.py
@@ -0,0 +1,9 @@
+from .imonitor import IMonitor
+from .process_monitor import ProcessMonitor
+from .network_monitor import NetworkMonitor
+
+__all__ = [
+    "IMonitor",
+    "ProcessMonitor",
+    "NetworkMonitor"
+]
