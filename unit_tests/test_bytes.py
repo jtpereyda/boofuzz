24cefb2348915cb5a57c3f475019bf79f175370f
stickler-ci <support@stickler-ci.com>
2020-04-16 21:35:04 +0000

    Fixing style errors.

--- unit_tests/test_bytes.py
+++ unit_tests/test_bytes.py
@@ -36,19 +36,19 @@ class TestBytes(unittest.TestCase):
                 actual_value = actual_callable(default_value)
                 expected_value = default_value[0:i] + expected_fuzz_string + default_value[i + 1 :]
                 self.assertEqual(expected_value, actual_value)
-        for i in range(0, len(default_default_value)-1):
+        for i in range(0, len(default_default_value) - 1):
             for expected_fuzz_string in b._fuzz_strings_2byte:
                 n += 1
                 actual_callable = next(generator)
                 actual_value = actual_callable(default_value)
-                expected_value = default_value[0:i] + expected_fuzz_string + default_value[i+2:]
+                expected_value = default_value[0:i] + expected_fuzz_string + default_value[i + 2 :]
                 self.assertEqual(expected_value, actual_value)
-        for i in range(0, len(default_default_value)-3):
+        for i in range(0, len(default_default_value) - 3):
             for expected_fuzz_string in b._fuzz_strings_4byte:
                 n += 1
                 actual_callable = next(generator)
                 actual_value = actual_callable(default_value)
-                expected_value = default_value[0:i] + expected_fuzz_string + default_value[i+4:]
+                expected_value = default_value[0:i] + expected_fuzz_string + default_value[i + 4 :]
                 self.assertEqual(expected_value, actual_value)
 
         self.assertEqual(n, b.num_mutations(default_value=default_value))
@@ -81,19 +81,19 @@ class TestBytes(unittest.TestCase):
                 actual_value = actual_callable(default_value)
                 expected_value = default_value[0:i] + expected_fuzz_string + default_value[i + 1 :]
                 self.assertEqual(truncate(expected_value), actual_value)
-        for i in range(0, len(default_default_value)-1):
+        for i in range(0, len(default_default_value) - 1):
             for expected_fuzz_string in b._fuzz_strings_2byte:
                 n += 1
                 actual_callable = next(generator)
                 actual_value = actual_callable(default_value)
-                expected_value = default_value[0:i] + expected_fuzz_string + default_value[i+2:]
+                expected_value = default_value[0:i] + expected_fuzz_string + default_value[i + 2 :]
                 self.assertEqual(truncate(expected_value), actual_value)
-        for i in range(0, len(default_default_value)-3):
+        for i in range(0, len(default_default_value) - 3):
             for expected_fuzz_string in b._fuzz_strings_4byte:
                 n += 1
                 actual_callable = next(generator)
                 actual_value = actual_callable(default_value)
-                expected_value = default_value[0:i] + expected_fuzz_string + default_value[i+4:]
+                expected_value = default_value[0:i] + expected_fuzz_string + default_value[i + 4 :]
                 self.assertEqual(truncate(expected_value), actual_value)
 
         self.assertEqual(n, b.num_mutations(default_value=default_value))
@@ -104,7 +104,7 @@ class TestBytes(unittest.TestCase):
 
         def fit_to_size(b):
             fullpad = b"\x41" * 5
-            pad_len = max(0, size-len(b))
+            pad_len = max(0, size - len(b))
             return b[0:size] + fullpad[0:pad_len]
 
         default_value = b"ABCDEFGH"
@@ -129,22 +129,23 @@ class TestBytes(unittest.TestCase):
                 actual_value = actual_callable(default_value)
                 expected_value = default_value[0:i] + expected_fuzz_string + default_value[i + 1 :]
                 self.assertEqual(fit_to_size(expected_value), actual_value)
-        for i in range(0, len(default_default_value)-1):
+        for i in range(0, len(default_default_value) - 1):
             for expected_fuzz_string in b._fuzz_strings_2byte:
                 n += 1
                 actual_callable = next(generator)
                 actual_value = actual_callable(default_value)
-                expected_value = default_value[0:i] + expected_fuzz_string + default_value[i+2:]
+                expected_value = default_value[0:i] + expected_fuzz_string + default_value[i + 2 :]
                 self.assertEqual(fit_to_size(expected_value), actual_value)
-        for i in range(0, len(default_default_value)-3):
+        for i in range(0, len(default_default_value) - 3):
             for expected_fuzz_string in b._fuzz_strings_4byte:
                 n += 1
                 actual_callable = next(generator)
                 actual_value = actual_callable(default_value)
-                expected_value = default_value[0:i] + expected_fuzz_string + default_value[i+4:]
+                expected_value = default_value[0:i] + expected_fuzz_string + default_value[i + 4 :]
                 self.assertEqual(fit_to_size(expected_value), actual_value)
 
         self.assertEqual(n, b.num_mutations(default_value=default_value))
+
 
 if __name__ == "__main__":
     unittest.main()
