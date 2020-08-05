fc41929704b309437fc97b8ce717edde1d8c9196
erik-smit <erik.lucas.smit@gmail.com>
2020-08-05 11:32:15 +0200

    s_group: fix compatibility with str input

--- boofuzz/primitives/group.py
+++ boofuzz/primitives/group.py
@@ -4,7 +4,7 @@ from .base_primitive import BasePrimitive
 
 
 class Group(BasePrimitive):
-    def __init__(self, name, values, default_value=None, *args, **kwargs):
+    def __init__(self, name, values, default_value=None, encoding="ascii", *args, **kwargs):
         """
         This primitive represents a list of static values, stepping through each one on mutation. You can tie a block
         to a group primitive to specify that the block should cycle through all possible mutations for *each* value
@@ -16,15 +16,19 @@ class Group(BasePrimitive):
         @param values:          List of possible raw values this group can take.
 
         @param default_value:   Specifying a value when fuzzing() is complete
+        @type  encoding:        str
+        @param encoding:        (Optional, def="ascii") String encoding, ex: utf_16_le for Microsoft Unicode.
         """
+        assert len(values) > 0, "You can't have an empty value list for your group!"
+        for val in values:
+            assert isinstance(val, (six.binary_type, six.string_types)), "Value list may only contain string/byte types"
+        values = list(map(lambda value: value if isinstance(value, bytes) else value.encode(encoding=encoding), values))
+
         if default_value is None:
             default_value = values[0]
         super(Group, self).__init__(name, default_value, *args, **kwargs)
 
         self.values = values
-        assert len(self.values) > 0, "You can't have an empty value list for your group!"
-        for val in self.values:
-            assert isinstance(val, (six.binary_type, six.string_types)), "Value list may only contain string/byte types"
 
     def mutations(self, default_value):
         for value in self.values:
