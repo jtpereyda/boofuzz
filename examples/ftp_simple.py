8090342e8a72ee44c95a90f930a45f1886f7aa4c
Fans0n <62650136+Fans0n-Fan@users.noreply.github.com>
2020-08-17 12:18:54 +0800

    Update ftp_simple.py

--- examples/ftp_simple.py
+++ examples/ftp_simple.py
@@ -2,15 +2,22 @@
 # Designed for use with boofuzz v0.2.0
 
 from boofuzz import *
-
-
-def main():
+    
+    
+    def main():
     """
     This example is a very simple FTP fuzzer. It uses no process monitory
     (procmon) and assumes that the FTP server is already running.
     """
-    session = Session(target=Target(connection=TCPSocketConnection("127.0.0.1", 21)))
-
+    port = 21
+    host = '127.0.0.1'
+    protocol = 'tcp'
+    session = Session(
+            target=Target(
+                connection = SocketConnection(host, port, proto=protocol),
+            ),
+    )
+    
     s_initialize("user")
     s_string("USER")
     s_delim(" ")
@@ -34,6 +41,14 @@ def main():
     s_delim(" ")
     s_string("AAAA")
     s_static("\r\n")
+
+    session.connect(s_get("user"))
+    session.connect(s_get("user"), s_get("pass"))
+    session.connect(s_get("pass"), s_get("stor"))
+    session.connect(s_get("pass"), s_get("retr"))
+
+    session.fuzz()
+
 
     session.connect(s_get("user"))
     session.connect(s_get("user"), s_get("pass"))
