35704e8a43d8069565f9207eedb0eeeb86087a63
stickler-ci <support@stickler-ci.com>
2020-08-17 07:44:10 +0000

    Fixing style errors.

--- examples/tftp_simple.py
+++ examples/tftp_simple.py
@@ -5,14 +5,10 @@ from boofuzz import *
 def main():
 
     port = 69
-    host = '127.0.0.1'
-    protocol = 'udp'
+    host = "127.0.0.1"
+    protocol = "udp"
 
-    session = Session(
-            target=Target(
-                connection = SocketConnection(host, port, proto=protocol),
-            ),
-    )
+    session = Session(target=Target(connection=SocketConnection(host, port, proto=protocol),),)
 
     s_initialize("RRQ")
     s_static("\x00\x01")
@@ -34,14 +30,12 @@ def main():
     s_static("\x00")
     s_static("mail")
     s_static("\x00")
-    
-    
 
     session.connect(s_get("WRQ"))
-    #session.connect(s_get("RRQ"))
-  
+    # session.connect(s_get("RRQ"))
 
     session.fuzz()
+
 
 if __name__ == "__main__":
 
