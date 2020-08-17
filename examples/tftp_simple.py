26e2525b8f26b1e48b4bd23473a1eadc0e7eeae7
Fans0n <62650136+Fans0n-Fan@users.noreply.github.com>
2020-08-17 18:21:18 +0800

    Add files via upload

---
+++ examples/tftp_simple.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# Designed for use with boofuzz v0.2.0
+
+from boofuzz import *
+
+
+def main():
+
+    port = 69
+    host = "127.0.0.1"
+    protocol = "udp"
+
+    session = Session(target=Target(connection=SocketConnection(host, port, proto=protocol),),)
+
+    s_initialize("RRQ")
+    s_static("\x00\x01")
+    s_string("filename")
+    s_static("\x00")
+    s_static("netascii")
+    s_static("\x00")
+
+    s_initialize("WRQ")
+    s_static("\x00\x02")
+    s_string("filename")
+    s_static("\x00")
+    s_static("netascii")
+    s_static("\x00")
+
+    s_initialize("TRQ")
+    s_static("\x00\x02")
+    s_string("filename")
+    s_static("\x00")
+    s_static("mail")
+    s_static("\x00")
+
+    session.connect(s_get("WRQ"))
+    # session.connect(s_get("RRQ"))
+
+    session.fuzz()
+    
+
+if __name__ == "__main__":
+
+    main()
