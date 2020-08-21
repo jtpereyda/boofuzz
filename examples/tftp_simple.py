67535a1117ea42e343860f8b9bf679e32382b787
Maximilian Lindner <46794237+SR4ven@users.noreply.github.com>
2020-08-22 01:30:36 +0200

    Minor adjustments

--- examples/tftp_simple.py
+++ examples/tftp_simple.py
@@ -5,40 +5,38 @@ from boofuzz import *
 
 
 def main():
-
     port = 69
     host = "127.0.0.1"
-    protocol = "udp"
 
-    session = Session(target=Target(connection=SocketConnection(host, port, proto=protocol),),)
+    session = Session(target=Target(connection=UDPSocketConnection(host, port),),)
 
     s_initialize("RRQ")
     s_static("\x00\x01")
-    s_string("filename")
+    s_string("filename", name="Filename")
     s_static("\x00")
-    s_static("netascii")
+    s_string("netascii", name="Mode")
     s_static("\x00")
 
     s_initialize("WRQ")
     s_static("\x00\x02")
-    s_string("filename")
+    s_string("filename", name="Filename")
     s_static("\x00")
-    s_static("netascii")
+    s_string("netascii", name="Mode")
     s_static("\x00")
 
     s_initialize("TRQ")
     s_static("\x00\x02")
-    s_string("filename")
+    s_string("filename", name="Filename")
     s_static("\x00")
     s_static("mail")
     s_static("\x00")
 
+    session.connect(s_get("RRQ"))
     session.connect(s_get("WRQ"))
-    # session.connect(s_get("RRQ"))
+    session.connect(s_get("TRQ"))
 
     session.fuzz()
 
 
 if __name__ == "__main__":
-
     main()
