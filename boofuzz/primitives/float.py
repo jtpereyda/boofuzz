6abb79dd3d71e7d6fb5c0947cd1c13a1672285b0
stickler-ci <support@stickler-ci.com>
2021-05-17 07:57:00 +0000

    Fixing style errors.

--- boofuzz/primitives/float.py
+++ boofuzz/primitives/float.py
@@ -1,10 +1,10 @@
 import random
 
 from ..fuzzable import Fuzzable
- 
+
 
 class Float(Fuzzable):
-    """Primitive that generates random float values within a specific range and with a fixed format. 
+    """Primitive that generates random float values within a specific range and with a fixed format.
 
     :type name: str, optional
     :param name: Name, for referencing lates. Names should always be provided, but if not a default name
@@ -19,20 +19,32 @@ class Float(Fuzzable):
     :type f_max: float, optional
     :param f_max: Maximal float value that can be generated while fuzzing, defaults to 100.0
     :type max_mutations: int, optional
-    :param max_mutations: Total number of mutations for this individual primitive, defaults to 1000 
+    :param max_mutations: Total number of mutations for this individual primitive, defaults to 1000
     :type seed: int or str or bytes or bytearray
     :param seed: Set random.seed() with the given seed for reproducible results
     """
-    def __init__(self, name=None, default_value:float=0.0, s_format:str='.1f', f_min:float=0.0, f_max:float=100.0, max_mutations:int=1000, seed=None, *args, **kwargs):
+
+    def __init__(
+        self,
+        name=None,
+        default_value: float = 0.0,
+        s_format: str = ".1f",
+        f_min: float = 0.0,
+        f_max: float = 100.0,
+        max_mutations: int = 1000,
+        seed=None,
+        *args,
+        **kwargs,
+    ):
         super(Float, self).__init__(name=name, default_value=str(default_value), *args, **kwargs)
-        
+
         self.s_format = s_format
         self.f_min = f_min
         self.f_max = f_max
         self.max_mutations = max_mutations
         self.seed = seed
-    
-    def mutations(self, default_value: float): 
+
+    def mutations(self, default_value: float):
         last_val = None
         if self.seed is not None:
             random.seed(self.seed)
@@ -43,16 +55,15 @@ class Float(Fuzzable):
             else:
                 current_val = random.uniform(self.f_min, self.f_max)
 
-            current_val = f'%{self.s_format}' % float(current_val)
+            current_val = f"%{self.s_format}" % float(current_val)
 
             if last_val == current_val:
                 continue
             last_val = current_val
-            yield current_val 
+            yield current_val
 
     def encode(self, value, mutation_context=None):
         return value.encode()
 
     def num_mutations(self, default_value):
         return self.max_mutations
-
--- examples/s_float.py
+++ examples/s_float.py
@@ -3,32 +3,41 @@ from boofuzz import *
 
 
 def main():
-    session = Session(target=Target(connection=UDPSocketConnection('127.0.0.1', 9999)))
+    session = Session(target=Target(connection=UDPSocketConnection("127.0.0.1", 9999)))
     define_floats(session)
     session.fuzz()
 
 
 def define_floats(session):
-    s_initialize('float-test')
+    s_initialize("float-test")
 
     # creating 10 float x with 5.0 <= x.x <= 10.0 and with random seed = 'first_seed'
-    s_float(10.0, f_min=5.0, f_max=15.0, max_mutations=10, seed='fist_seed', fuzzable=True, name='first_float')
-    s_static(',')
+    s_float(10.0, f_min=5.0, f_max=15.0, max_mutations=10, seed="fist_seed", fuzzable=True, name="first_float")
+    s_static(",")
 
     # creating 10 floats x with 10.0 <= x.xx <= 20.0 adn with random seed = 'second_seed'
-    s_float(12.34, s_format='.2f', f_min=10.0, f_max=20.0, max_mutations=10, seed='second_seed', fuzzable=True, name='second_float')
-    s_static(',')
+    s_float(
+        12.34,
+        s_format=".2f",
+        f_min=10.0,
+        f_max=20.0,
+        max_mutations=10,
+        seed="second_seed",
+        fuzzable=True,
+        name="second_float",
+    )
+    s_static(",")
 
-    # creating 25 floats x with 20.0 <= x.xxxx <= 40.0, without a random seed 
-    s_float(25.1234, f_min=20.0, f_max=40.0, max_mutations=25, fuzzable=True, name='third_float')
-    s_static(',')
+    # creating 25 floats x with 20.0 <= x.xxxx <= 40.0, without a random seed
+    s_float(25.1234, f_min=20.0, f_max=40.0, max_mutations=25, fuzzable=True, name="third_float")
+    s_static(",")
 
     # creates a float, that will not be fuzzed. The Value is static.
-    s_float(42.0, fuzzable=False, name='static_float_value')
-    s_static('\r\n')
+    s_float(42.0, fuzzable=False, name="static_float_value")
+    s_static("\r\n")
 
-    session.connect(s_get('float-test'))
+    session.connect(s_get("float-test"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
