d5ddb062f16e50a4214905ad7c6901e28991ecd1
Katharina Bogad <katharina@hacked.xyz>
2022-08-04 22:04:40 +0200

    Split boofuzz/sessions.py

---
+++ boofuzz/sessions/__init__.py
@@ -0,0 +1,7 @@
+from .connection import Connection
+from .session_info import SessionInfo
+from .session import Session, open_test_run
+from .target import Target
+from .web_app import WebApp
+
+__all__ = [ Connection, SessionInfo, Target, Session, WebApp, open_test_run ]
